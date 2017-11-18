79b9e5c6e9844a9755623650733962a60c8e8102
Donatas <donatas@pixelneat.com>
2017-11-18 21:27:51 +0200

    added image with more quality, and positions of the header

--- src/blocks/about.js
+++ src/blocks/about.js
@@ -1,15 +1,20 @@
 import React, {Component} from 'react';
 
+const blockData = {
+    title: "About me",
+    content: "I'm professional freelance full stack web developer working with back-end & front-end sides. I have experience with various " +
+    "PHP web frameworks, content management systems and user interface interactions. I‘ve gained Bachelors degree of network and system " +
+    "administrator. I am highly motivated and result-oriented professional."
+};
+
 class About extends Component {
     render() {
         return (
             <section className="content-block about-block">
                 <div className="about-block--container mdc-layout-grid">
-                    <h1 className="about-block--title">{'About me'}</h1>
-
+                    <h1 className="about-block--title">{blockData.title}</h1>
                     <div className="about-block--content mdc-layout-grid__cell">
-                        <p>
-                            I'm professional freelance full stack web developer working with back-end & front-end sides. I have experience with various PHP web frameworks, content management systems and user interface interactions. I‘ve gained Bachelors degree of network and system administrator. I am highly motivated and result-oriented professional.</p>
+                        <p>{blockData.content}</p>
                     </div>
                 </div>
             </section>
--- src/partials/header.js
+++ src/partials/header.js
@@ -4,21 +4,17 @@ import {Link, NavLink} from 'react-router-dom';
 class Header extends Component {
     render() {
         return (
-            <header className="mdc-layout-grid header" role="banner">
-                <div className="mdc-layout-grid__inner">
-                    <div
-                        className="mdc-layout-grid__cell mdc-layout-grid__cell--span-4-tablet mdc-layout-grid__cell--span-6 mdc-layout-grid--align-left header__logo">
-                        <Link to="/">{'Donatas Navidonskis'}</Link>
-                    </div>
-                    <nav
-                        className="mdc-layout-grid__cell mdc-layout-grid__cell--span-4-tablet mdc-layout-grid__cell--span-6 mdc-layout-grid--align-right header__navigation">
-                        <NavLink to="/about" activeClassName="active">{'About'}</NavLink>
-                        <NavLink to="/blog" activeClassName="active">{'Blog'}</NavLink>
-                        <NavLink to="/contacts"
-                                 className="mdc-button mdc-button--raised"
-                                 activeClassName="active">{'Contacts'}</NavLink>
-                    </nav>
+            <header className="header" role="banner">
+                <div className="header__logo">
+                    <Link to="/">{'Donatas Navidonskis'}</Link>
                 </div>
+                <nav className="header__navigation">
+                    <NavLink to="/about" activeClassName="active">{'About'}</NavLink>
+                    <NavLink to="/blog" activeClassName="active">{'Blog'}</NavLink>
+                    <NavLink to="/contacts"
+                             className="mdc-button mdc-button--raised"
+                             activeClassName="active">{'Contacts'}</NavLink>
+                </nav>
             </header>
         );
     }
