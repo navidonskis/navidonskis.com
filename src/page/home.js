2f62a35b65b3f56817f4fffcf72c9664847edd63
Donatas <donatas.n@estina.lt>
2018-02-11 22:01:09 +0200

    home component page content updated

--- src/page/home.js
+++ src/page/home.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Layout from '../component/layout';
+import SwappedSkills from '../component/swapped.skills';
 
 class Home extends React.Component {
 
@@ -7,10 +8,9 @@ class Home extends React.Component {
     return (
       <Layout className="home">
         <div className="home__container">
-          <h2 className="home--title padded-multiline">
-            <span>
-              <span>Hello, my name is Donatas and i'm a Software Engineer from</span>
-            </span>
+          <h2 className="home--title">
+            Hello, my name is Donatas and I'm a <SwappedSkills timeout={5000} /> from
+            <a href="https://goo.gl/mdXy2c" target="_blank" rel="noopener noreferrer">Vilnius, Lithuania</a>
           </h2>
         </div>
       </Layout>
