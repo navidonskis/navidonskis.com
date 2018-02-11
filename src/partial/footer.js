11dbed0e62d3544e97dd1c6a12be210ec109acee
Donatas <donatas.n@estina.lt>
2018-02-11 22:01:17 +0200

    footer added

--- src/partial/footer.js
+++ src/partial/footer.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
+import SocialLinks from '../component/social.links';
 
 class Footer extends React.Component {
 
   render() {
     return (
       <footer className="footer">
-        &copy; navidonskis.com
+        <div className="footer__container">
+          <SocialLinks />
+        </div>
       </footer>
     );
   }
