335a762e7ccd9ec023fd96336fdaf47d1d4a7971
Donatas <donatas.n@estina.lt>
2018-08-14 12:29:30 +0300

    blog, home , typography

--- src/page/blog.js
+++ src/page/blog.js
@@ -1,12 +1,14 @@
 import React, {Component} from "react";
 import Layout from "../component/layout";
 
+// https://material-components.github.io/material-components-web-catalog/#/component/typography
+// https://github.com/material-components/material-components-web/tree/master/packages/mdc-typography
 class Blog extends Component {
 
     render() {
         return (
             <Layout className="blog">
-                <h1 className="mdc-typography--headline2">Blog</h1>
+                <h1 className="mdc-typography--headline1">Blog</h1>
             </Layout>
         );
     }
