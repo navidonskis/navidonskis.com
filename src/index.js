1c13c24768d5b0fad6a9b025937ddffce2fa164c
Donatas <donatas.n@estina.lt>
2018-08-26 00:21:08 +0300

    fixes of the import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+export default App;
+
 // Render your app
 if (typeof document !== 'undefined') {
   const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate || ReactDOM.render;
