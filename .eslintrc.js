9afa0db3ba461415219e3272a26945b95ed8ef40
Donatas <donatas.n@estina.lt>
2018-08-26 00:17:59 +0300

    configs

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: 'react-tools',
+    rules: {
+        "semi": 0,
+        "react/jsx-indent": 4,
+        "react/jsx-tag-spacing": 0,
+    }
+};
---
+++ static.config.js
@@ -0,0 +1,102 @@
+import { reloadRoutes } from 'react-static/node';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import jdown from 'jdown';
+import chokidar from 'chokidar';
+
+chokidar.watch('content').on('all', () => reloadRoutes());
+
+export default {
+  getSiteData: () => ({
+    title: 'React Static',
+  }),
+  getRoutes: async () => {
+    const { posts, home, about } = await jdown('content');
+
+    return [
+      {
+        path: '/',
+        component: 'src/containers/Home',
+        getData: () => ({
+          home,
+        }),
+      },
+      {
+        path: '/about',
+        component: 'src/containers/About',
+        getData: () => ({
+          about,
+        }),
+      },
+      {
+        path: '/blog',
+        component: 'src/containers/Blog',
+        getData: () => ({
+          posts,
+        }),
+        children: posts.map(post => ({
+          path: `/post/${post.slug}`,
+          component: 'src/containers/Post',
+          getData: () => ({
+            post,
+          }),
+        })),
+      },
+      {
+        is404: true,
+        component: 'src/containers/404',
+      },
+    ]
+  },
+  webpack: (config, { defaultLoaders, stage }) => {
+    let loaders = [];
+
+    if (stage === 'dev') {
+      loaders = [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }];
+    } else {
+      loaders = [
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+            minimize: stage === 'prod',
+            sourceMap: false,
+          },
+        },
+        {
+          loader: 'sass-loader',
+          options: { includePaths: ['src/'] },
+        },
+      ];
+
+      // Don't extract css to file during node build process
+      if (stage !== 'node') {
+        loaders = ExtractTextPlugin.extract({
+          fallback: {
+            loader: 'style-loader',
+            options: {
+              sourceMap: false,
+              hmr: false,
+            },
+          },
+          use: loaders,
+        });
+      }
+    }
+
+    config.module.rules = [
+      {
+        oneOf: [
+          {
+            test: /\.s(a|c)ss$/,
+            use: loaders,
+          },
+          defaultLoaders.cssLoader,
+          defaultLoaders.jsLoader,
+          defaultLoaders.fileLoader,
+        ],
+      },
+    ];
+
+    return config;
+  },
+};
