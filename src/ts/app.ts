3cbe6056d59685213f7bffbadec49b8528a3d5be
Donatas <donatas.n@estina.lt>
2019-09-15 17:43:59 +0300

    reorganizing js with ts and revert back app.min.js

---
+++ src/ts/app.ts
@@ -0,0 +1,11 @@
+import { XElement } from "./libs/xelement";
+
+const layout = new XElement(document.querySelector('.layout'));
+
+layout.addClass('laba-diena');
+
+console.log('hello, world');
+
+const testMessage: string = 'TypeScript works';
+
+console.log(testMessage);
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
 const path = require('path');
 
 module.exports = {
-  'entry': './assets/js/app.js',
+  'entry': {
+    'app': './src/ts/app.ts'
+  },
+  'resolve': {
+    'extensions': ['.ts', '.js']
+  },
   'output': {
     'path': path.resolve(__dirname, 'assets/js'),
-    'filename': 'app.min.js'
+    'filename': '[name].min.js'
+  },
+  'devtool': 'source-map',
+  'devServer': {
+    'open': true,
   },
   'module': {
     'rules': [
       {
-        'test': /\.js$/,
-        'exclude': /node_modules/,
-        'use': {
-          'loader': "babel-loader",
-          'options': {
-            'presets': ["@babel/preset-env"]
-          }
-        }
+        'test': /\.ts?$/,
+        'use': 'ts-loader',
+        'exclude': /node_modules/
       }
     ]
-  }
+  },
 };
